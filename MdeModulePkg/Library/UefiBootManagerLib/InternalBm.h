8b738129b94c07a464acbe91181b2699d90f28c4
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-06-16 08:50:52 +0200

    MdeModulePkg: Be more verbose about SecureBoot and the reason why the boot failed
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- MdeModulePkg/Library/UefiBootManagerLib/InternalBm.h
+++ MdeModulePkg/Library/UefiBootManagerLib/InternalBm.h
@@ -44,6 +44,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Protocol/PlatformBootManager.h>
 #include <Protocol/PartitionInfo.h>
 
+#include <Guid/ImageAuthentication.h>
 #include <Guid/MemoryTypeInformation.h>
 #include <Guid/FileInfo.h>
 #include <Guid/GlobalVariable.h>
