9ccb9a20b903309f31d072d3a6f717e8681d906b
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-07-22 11:08:57 +0200

    DasharoPayloadPkg/BlSupportDxe: Use EfiSmbiosProtocol to install tables
    The default EfiSmbiosProtocol operates on an empty SMBIOS tables.
    
    As the SMBIOS tables are provided by the bootloader, install the
    SMBIOS tables using the EfiSmbiosProtocol to make them appear in the
    UI, which until now doesn't display hardware information.
    
    Tests showed that the OS can still see the SMBIOS tables.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- DasharoPayloadPkg/BlSupportDxe/BlSupportDxe.c
+++ DasharoPayloadPkg/BlSupportDxe/BlSupportDxe.c
@@ -79,6 +79,109 @@ ReserveResourceInGcd (
   return Status;
 }
 
+EFI_STATUS
+EFIAPI
+BlDxeInstallSMBIOStables(
+  IN UINT64    SmbiosTableBase,
+  IN UINT32    SmbiosTableSize
+)
+{
+  EFI_STATUS                    Status;
+  SMBIOS_TABLE_ENTRY_POINT      *SmbiosTable;
+  SMBIOS_TABLE_3_0_ENTRY_POINT  *Smbios30Table;
+  SMBIOS_STRUCTURE_POINTER      Smbios;
+  SMBIOS_STRUCTURE_POINTER      SmbiosEnd;
+  CHAR8                         *String;
+  EFI_SMBIOS_HANDLE             SmbiosHandle;
+  EFI_SMBIOS_PROTOCOL           *SmbiosProto;
+
+  //
+  // Locate Smbios protocol.
+  //
+  Status = gBS->LocateProtocol (&gEfiSmbiosProtocolGuid, NULL, (VOID **)&SmbiosProto);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to locate gEfiSmbiosProtocolGuid\n",
+        __FUNCTION__
+    ));
+    return Status;
+  }
+
+  Smbios30Table = (SMBIOS_TABLE_3_0_ENTRY_POINT *)(UINTN)(SmbiosTableBase);
+  SmbiosTable = (SMBIOS_TABLE_ENTRY_POINT *)(UINTN)(SmbiosTableBase);
+
+  if (CompareMem (Smbios30Table->AnchorString, "_SM3_", 5) == 0) {
+    Smbios.Hdr = (SMBIOS_STRUCTURE *) (UINTN) Smbios30Table->TableAddress;
+    SmbiosEnd.Raw = (UINT8 *) (UINTN) (Smbios30Table->TableAddress + Smbios30Table->TableMaximumSize);
+    if (Smbios30Table->TableMaximumSize > SmbiosTableSize) {
+      DEBUG((EFI_D_INFO, "%a: SMBIOS table size greater than reported by bootloader\n",
+          __FUNCTION__));
+    }
+  } else if (CompareMem (SmbiosTable->AnchorString, "_SM_", 4) == 0) {
+    Smbios.Hdr    = (SMBIOS_STRUCTURE *) (UINTN) SmbiosTable->TableAddress;
+    SmbiosEnd.Raw = (UINT8 *) ((UINTN) SmbiosTable->TableAddress + SmbiosTable->TableLength);
+
+    if (SmbiosTable->TableLength > SmbiosTableSize) {
+      DEBUG((EFI_D_INFO, "%a: SMBIOS table size greater than reported by bootloader\n",
+          __FUNCTION__
+      ));
+    }
+  } else {
+    DEBUG ((DEBUG_ERROR, "%a: No valid SMBIOS table found\n",
+        __FUNCTION__
+    ));
+    return EFI_NOT_FOUND;
+  }
+
+  do {
+    // Check for end marker
+    if (Smbios.Hdr->Type == 127) {
+      break;
+    }
+
+    // Install the table
+    SmbiosHandle = SMBIOS_HANDLE_PI_RESERVED;
+    Status = SmbiosProto->Add (
+                          SmbiosProto,
+                          gImageHandle,
+                          &SmbiosHandle,
+                          Smbios.Hdr
+                          );
+    ASSERT_EFI_ERROR (Status);
+
+    //
+    // Go to the next SMBIOS structure. Each SMBIOS structure may include 2 parts:
+    // 1. Formatted section; 2. Unformatted string section. So, 2 steps are needed
+    // to skip one SMBIOS structure.
+    //
+
+    //
+    // Step 1: Skip over formatted section.
+    //
+    String = (CHAR8 *) (Smbios.Raw + Smbios.Hdr->Length);
+
+    //
+    // Step 2: Skip over unformatted string section.
+    //
+    do {
+      //
+      // Each string is terminated with a NULL(00h) BYTE and the sets of strings
+      // is terminated with an additional NULL(00h) BYTE.
+      //
+      for ( ; *String != 0; String++) {
+      }
+
+      if (*(UINT8*)++String == 0) {
+        //
+        // Pointer to the next SMBIOS structure.
+        //
+        Smbios.Raw = (UINT8 *)++String;
+        break;
+      }
+    } while (TRUE);
+  } while (Smbios.Raw < SmbiosEnd.Raw);
+
+  return EFI_SUCCESS;
+}
 
 /**
   Main entry for the bootloader support DXE module.
@@ -133,8 +236,12 @@ BlDxeEntryPoint (
   //
   if (SystemTableInfo->SmbiosTableBase != 0 && SystemTableInfo->SmbiosTableSize != 0) {
     DEBUG ((DEBUG_ERROR, "Install Smbios Table at 0x%lx, length 0x%x\n", SystemTableInfo->SmbiosTableBase, SystemTableInfo->SmbiosTableSize));
-    Status = gBS->InstallConfigurationTable (&gEfiSmbiosTableGuid, (VOID *)(UINTN)SystemTableInfo->SmbiosTableBase);
-    ASSERT_EFI_ERROR (Status);
+
+    if (BlDxeInstallSMBIOStables(SystemTableInfo->SmbiosTableBase,
+        SystemTableInfo->SmbiosTableSize) != EFI_SUCCESS) {
+      Status = gBS->InstallConfigurationTable (&gEfiSmbiosTableGuid, (VOID *)(UINTN)SystemTableInfo->SmbiosTableBase);
+      ASSERT_EFI_ERROR (Status);
+    }
   }
 
   //
