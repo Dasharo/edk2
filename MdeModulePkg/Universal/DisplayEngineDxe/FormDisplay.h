ec70ec59b631e35962028f65bdd692963f962e41
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-05-24 19:03:00 +0300

    MdeModulePkg: move GetStringWidth() to CustomizedDisplayLib
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- MdeModulePkg/Universal/DisplayEngineDxe/FormDisplay.h
+++ MdeModulePkg/Universal/DisplayEngineDxe/FormDisplay.h
@@ -486,27 +486,6 @@ GetToken (
   );
 
 /**
-  Count the storage space of a Unicode string.
-
-  This function handles the Unicode string with NARROW_CHAR
-  and WIDE_CHAR control characters. NARROW_HCAR and WIDE_CHAR
-  does not count in the resultant output. If a WIDE_CHAR is
-  hit, then 2 Unicode character will consume an output storage
-  space with size of CHAR16 till a NARROW_CHAR is hit.
-
-  If String is NULL, then ASSERT ().
-
-  @param String          The input string to be counted.
-
-  @return Storage space for the input string.
-
-**/
-UINTN
-GetStringWidth (
-  IN CHAR16               *String
-  );
-
-/**
   This routine reads a numeric value from the user input.
 
   @param  MenuOption        Pointer to the current input menu.
