347b6b293d2a736b9799b945238d26faed600242
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-09-04 16:01:58 +0200

    MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciHostBridge.c: Set WC memory for prefetchable resources
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciHostBridge.c
+++ MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciHostBridge.c
@@ -392,6 +392,7 @@ InitializePciHostBridge (
   BOOLEAN                     ResourceAssigned;
   LIST_ENTRY                  *Link;
   UINT64                      HostAddress;
+  UINT64                      MemType;
 
   RootBridges = PciHostBridgeGetRootBridges (&RootBridgeCount);
   if ((RootBridges == NULL) || (RootBridgeCount == 0)) {
@@ -474,6 +475,12 @@ InitializePciHostBridge (
 
     for (MemApertureIndex = 0; MemApertureIndex < ARRAY_SIZE (MemApertures); MemApertureIndex++) {
       if (MemApertures[MemApertureIndex]->Base <= MemApertures[MemApertureIndex]->Limit) {
+        // UC for Memory aperture and WC for Prefetchable aperture
+        if (MemApertureIndex > 1) {
+          MemType = EFI_MEMORY_WC;
+        } else {
+          MemType = EFI_MEMORY_UC;
+        }
         //
         // Base and Limit in PCI_ROOT_BRIDGE_APERTURE are device address.
         // For GCD resource manipulation, we need to use host address.
@@ -483,16 +490,17 @@ InitializePciHostBridge (
         Status = AddMemoryMappedIoSpace (
                    HostAddress,
                    MemApertures[MemApertureIndex]->Limit - MemApertures[MemApertureIndex]->Base + 1,
-                   EFI_MEMORY_UC
+                   MemType
                    );
         ASSERT_EFI_ERROR (Status);
         Status = gDS->SetMemorySpaceAttributes (
                         HostAddress,
                         MemApertures[MemApertureIndex]->Limit - MemApertures[MemApertureIndex]->Base + 1,
-                        EFI_MEMORY_UC
+                        MemType
                         );
         if (EFI_ERROR (Status)) {
-          DEBUG ((DEBUG_WARN, "PciHostBridge driver failed to set EFI_MEMORY_UC to MMIO aperture - %r.\n", Status));
+          DEBUG ((DEBUG_WARN, "PciHostBridge driver failed to set EFI_MEMORY_%s to MMIO aperture %d - %r.\n",
+                              MemType == EFI_MEMORY_WC ? L"WC" : L"UC", MemApertureIndex, Status));
         }
         if (ResourceAssigned) {
           Status = gDS->AllocateMemorySpace (
