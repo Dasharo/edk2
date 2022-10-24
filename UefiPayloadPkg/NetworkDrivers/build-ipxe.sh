a9002cd3beddcc4b62c787faaf47310f1fa8218f
Piotr Król <piotr.krol@3mdeb.com>
2022-10-25 01:13:58 +0200

    UefiPayloadPkg/NetworkDrivers/build-ipxe.sh: use ipxe master and sed for config changes
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
+++ UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 rm -rf ipxe/
-git clone git@github.com:Dasharo/ipxe.git -b v1.21.1
+git clone https://github.com/ipxe/ipxe.git
+
+cd ipxe/src
+sed 's|.*DOWNLOAD_PROTO_HTTPS|#define DOWNLOAD_PROTO_HTTPS|g' config/general.h > config/general.h.tmp
+sed 's|//#define\s*IMAGE_BZIMAGE.*|#define IMAGE_BZIMAGE|' config/general.h > config/general.h.tmp
+sed 's|//#define\s*IMAGE_GZIP.*|#define IMAGE_GZIP|' config/general.h > config/general.h.tmp
+sed 's|//#define\s*NSLOOKUP_CMD.*|#define NSLOOKUP_CMD|' config/general.h > config/general.h.tmp
+sed 's|//#define\s*IMAGE_TRUST_CMD.*|#define IMAGE_TRUST_CMD|' config/general.h > config/general.h.tmp
+sed 's|//#define\s*PING_CMD.*|#define PING_CMD|' config/general.h > config/general.h.tmp
+mv config/general.h.tmp config/general.h
 
 cp dasharo.ipxe ipxe/src
 cd ipxe/src
