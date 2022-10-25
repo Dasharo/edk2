3eb1b94febd0afbe4611e8104d199dce11005d9e
Michał Kopeć <michal.kopec@3mdeb.com>
2022-10-25 17:56:29 +0200

    DasharoModulePkg: UserAuthenticationDxeSmm: Convert to pure DXE driver
    Remove SMM dependency as we can't use it with coreboot
    
    Co-authored-by: Filip Gołaś <filip.golas@3mdeb.com>
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- DasharoModulePkg/Include/Guid/UserAuthentication.h
+++ DasharoModulePkg/Include/Guid/UserAuthentication.h
@@ -12,34 +12,42 @@
 #define PASSWORD_MIN_SIZE    9  // MIN number of chars of password, including NULL.
 #define PASSWORD_MAX_SIZE    33 // MAX number of chars of password, including NULL.
 
+#define PASSWORD_SALT_SIZE   32
+#define PASSWORD_HASH_SIZE   32 // SHA256_DIGEST_SIZE
+
+#define PASSWORD_MAX_TRY_COUNT  3
+#define PASSWORD_HISTORY_CHECK_COUNT  5
+
+//
+// Name of the variable
+//
+#define USER_AUTHENTICATION_VAR_NAME L"Password"
+#define USER_AUTHENTICATION_HISTORY_LAST_VAR_NAME L"PasswordLast"
+
+//
+// Variable storage
+//
+typedef struct {
+  UINT8        PasswordHash[PASSWORD_HASH_SIZE];
+  UINT8        PasswordSalt[PASSWORD_SALT_SIZE];
+} USER_PASSWORD_VAR_STRUCT;
+
 #define USER_AUTHENTICATION_GUID \
   { 0xf06e3ea7, 0x611c, 0x4b6b, { 0xb4, 0x10, 0xc2, 0xbf, 0x94, 0x3f, 0x38, 0xf2 } }
 
 extern EFI_GUID gUserAuthenticationGuid;
 
 typedef struct {
-  UINTN       Function;
-  EFI_STATUS  ReturnStatus;
-} SMM_PASSWORD_COMMUNICATE_HEADER;
-
-#define SMM_PASSWORD_FUNCTION_IS_PASSWORD_SET       1
-#define SMM_PASSWORD_FUNCTION_SET_PASSWORD          2
-#define SMM_PASSWORD_FUNCTION_VERIFY_PASSWORD       3
-#define SMM_PASSWORD_FUNCTION_SET_VERIFY_POLICY     4
-#define SMM_PASSWORD_FUNCTION_GET_VERIFY_POLICY     5
-#define SMM_PASSWORD_FUNCTION_WAS_PASSWORD_VERIFIED 6
-
-typedef struct {
   CHAR8                                 NewPassword[PASSWORD_MAX_SIZE];
   CHAR8                                 OldPassword[PASSWORD_MAX_SIZE];
-} SMM_PASSWORD_COMMUNICATE_SET_PASSWORD;
+} PASSWORD_COMMUNICATE_SET_PASSWORD;
 
 typedef struct {
   CHAR8                                 Password[PASSWORD_MAX_SIZE];
-} SMM_PASSWORD_COMMUNICATE_VERIFY_PASSWORD;
+} PASSWORD_COMMUNICATE_VERIFY_PASSWORD;
 
 typedef struct {
   BOOLEAN                               NeedReVerify;
-} SMM_PASSWORD_COMMUNICATE_VERIFY_POLICY;
+} PASSWORD_COMMUNICATE_VERIFY_POLICY;
 
 #endif
