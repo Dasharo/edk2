d26f3530535861543870aae8f53082313ae4e94c
Piotr Król <piotr.krol@3mdeb.com>
2022-10-26 00:52:25 +0200

    UefiPayloadPkg/NetworkDrivers/build-ipxe.sh: use dasharo/skini_lz-next and disable CONFIG_BZIMAGE
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
+++ UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 rm -rf ipxe/
-git clone https://github.com/ipxe/ipxe.git
+git clone https://github.com/Dasharo/ipxe.git -b skinit_lz-next
 
 cd ipxe/src
 sed -i 's|.*DOWNLOAD_PROTO_HTTPS|#define DOWNLOAD_PROTO_HTTPS|g' config/general.h
 sed -i 's|//#define\s*IMAGE_MULTIBOOT2.*|#define IMAGE_MULTIBOOT2|' config/general.h
-sed -i 's|//#define\s*IMAGE_BZIMAGE.*|#define IMAGE_BZIMAGE|' config/general.h
+# sed -i 's|//#define\s*IMAGE_BZIMAGE.*|#define IMAGE_BZIMAGE|' config/general.h
 sed -i 's|//#define\s*IMAGE_GZIP.*|#define IMAGE_GZIP|' config/general.h
 sed -i 's|//#define\s*NSLOOKUP_CMD.*|#define NSLOOKUP_CMD|' config/general.h
 sed -i 's|//#define\s*IMAGE_TRUST_CMD.*|#define IMAGE_TRUST_CMD|' config/general.h
