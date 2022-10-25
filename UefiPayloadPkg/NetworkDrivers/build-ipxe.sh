f5ce48038fed9447b0fcc78dadf2c4d42af7c869
Piotr Król <piotr.krol@3mdeb.com>
2022-10-25 18:23:56 +0200

    UefiPayloadPkg/NetworkDrivers/build-ipxe.sh: correct sed command
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
+++ UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
@@ -4,13 +4,13 @@ rm -rf ipxe/
 git clone https://github.com/ipxe/ipxe.git
 
 cd ipxe/src
-sed 's|.*DOWNLOAD_PROTO_HTTPS|#define DOWNLOAD_PROTO_HTTPS|g' config/general.h > config/general.h.tmp
-sed 's|//#define\s*IMAGE_BZIMAGE.*|#define IMAGE_BZIMAGE|' config/general.h > config/general.h.tmp
-sed 's|//#define\s*IMAGE_GZIP.*|#define IMAGE_GZIP|' config/general.h > config/general.h.tmp
-sed 's|//#define\s*NSLOOKUP_CMD.*|#define NSLOOKUP_CMD|' config/general.h > config/general.h.tmp
-sed 's|//#define\s*IMAGE_TRUST_CMD.*|#define IMAGE_TRUST_CMD|' config/general.h > config/general.h.tmp
-sed 's|//#define\s*PING_CMD.*|#define PING_CMD|' config/general.h > config/general.h.tmp
-mv config/general.h.tmp config/general.h
+sed -i 's|.*DOWNLOAD_PROTO_HTTPS|#define DOWNLOAD_PROTO_HTTPS|g' config/general.h
+sed -i 's|//#define\s*IMAGE_MULTIBOOT2.*|#define IMAGE_MULTIBOOT2|' config/general.h
+sed -i 's|//#define\s*IMAGE_BZIMAGE.*|#define IMAGE_BZIMAGE|' config/general.h
+sed -i 's|//#define\s*IMAGE_GZIP.*|#define IMAGE_GZIP|' config/general.h
+sed -i 's|//#define\s*NSLOOKUP_CMD.*|#define NSLOOKUP_CMD|' config/general.h
+sed -i 's|//#define\s*IMAGE_TRUST_CMD.*|#define IMAGE_TRUST_CMD|' config/general.h
+sed -i 's|//#define\s*PING_CMD.*|#define PING_CMD|' config/general.h
 
 cp ../../dasharo.ipxe .
 make bin-x86_64-efi/ipxe.efi EMBED=dasharo.ipxe
