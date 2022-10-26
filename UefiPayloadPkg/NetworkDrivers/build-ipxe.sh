0abbc8cbad6c0aab0018ebfbf0110a1b50b1d3ed
Piotr Król <piotr.krol@3mdeb.com>
2022-10-26 16:36:34 +0200

    UefiPayloadPkg/NetworkDrivers/build-ipxe.sh: enable serial console logs
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
+++ UefiPayloadPkg/NetworkDrivers/build-ipxe.sh
@@ -11,6 +11,7 @@ sed -i 's|//#define\s*IMAGE_GZIP.*|#define IMAGE_GZIP|' config/general.h
 sed -i 's|//#define\s*NSLOOKUP_CMD.*|#define NSLOOKUP_CMD|' config/general.h
 sed -i 's|//#define\s*IMAGE_TRUST_CMD.*|#define IMAGE_TRUST_CMD|' config/general.h
 sed -i 's|//#define\s*PING_CMD.*|#define PING_CMD|' config/general.h
+sed -i 's|//#define\s*CONSOLE_SERIAL.*|#define CONSOLE_SERIAL|' config/console.h
 
 cp ../../dasharo.ipxe .
 make bin-x86_64-efi/ipxe.efi EMBED=dasharo.ipxe
