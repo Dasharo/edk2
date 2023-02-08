6fa51d433121e0d364567f6a088e8267f68bdc7b
Kacper Stojek <kacper.stojek@3mdeb.com>
2023-02-08 09:25:01 +0100

    DasharoModulePkg: Add DmaProtection and IommuHandoff options
    Signed-off-by: Kacper Stojek <kacper.stojek@3mdeb.com>
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- DasharoModulePkg/DasharoBootPolicies/BootPolicies.c
+++ DasharoModulePkg/DasharoBootPolicies/BootPolicies.c
@@ -8,10 +8,12 @@ Copyright (c)  1999  - 2014, Intel Corporation. All rights reserved
 /** @file
 **/
 
+#include <Library/PcdLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/UefiLib.h>
 #include "BootPolicies.h"
+#include "Library/DasharoSystemFeaturesUiLib/DasharoSystemFeaturesHii.h"
 
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
@@ -38,6 +40,8 @@ InitializeBootPolicies (
   EFI_STATUS  Status = EFI_SUCCESS;
   BOOLEAN *EfiVar;
   UINTN VarSize = sizeof(BOOLEAN);
+  IOMMU_CONFIG *IommuConfig;
+  UINT8 PcdVal = 0;
 
   gBS = SystemTable->BootServices;
   gRT = SystemTable->RuntimeServices;
@@ -142,6 +146,36 @@ InitializeBootPolicies (
       NULL
       );
     DEBUG ((EFI_D_INFO, "Boot Policy: Enabling PS2 Controller\n"));
+  }
+
+  VarSize = sizeof(*IommuConfig);
+  Status = GetVariable2 (
+           L"IommuConfig",
+           &gDasharoSystemFeaturesGuid,
+           (VOID **) &IommuConfig,
+           &VarSize
+           );
+
+  if ((Status == EFI_SUCCESS) && (VarSize == sizeof(*IommuConfig))){
+    PcdVal = PcdGet8(PcdVTdPolicyPropertyMask);
+    if (IommuConfig->IommuEnable){
+      PcdVal |= 0x01;
+      if (IommuConfig->IommuHandoff){
+        PcdVal |= 0x02;
+        DEBUG ((EFI_D_INFO, "Boot Policy: IOMMU will be kept enabled on ExitBootServices\n"));
+      }
+      else{
+        PcdVal &= (~0x02);
+        DEBUG ((EFI_D_INFO, "Boot Policy: IOMMU will be disabled on ExitBootServices\n"));
+      }
+      PcdSet8S(PcdVTdPolicyPropertyMask, PcdVal);
+    } else {
+      PcdSet8S(PcdVTdPolicyPropertyMask, PcdVal & (~0x03));
+      DEBUG ((EFI_D_INFO, "Boot Policy: DMA protection disabled\n"));
+    }
+  } else {
+    PcdSet8S(PcdVTdPolicyPropertyMask, PcdVal & (~0x03));
+    DEBUG ((EFI_D_INFO, "Boot Policy: DMA protection disabled\n"));
   }
 
   return EFI_SUCCESS;
