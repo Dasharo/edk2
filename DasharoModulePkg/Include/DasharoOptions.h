42071f99f4f5d5348ce1715423ce1c06b77f8df1
Husni Faiz <husni.faiz@3mdeb.com>
2023-08-29 03:01:20 +0530

    DasharoModulePkg: DasharoSystemFeatures: add PCI OptionROM execution option

---
+++ DasharoModulePkg/Include/DasharoOptions.h
@@ -0,0 +1,16 @@
+/** @file
+Constants for options of Dasharo system features
+
+Copyright (c) 2023, 3mdeb Sp. z o.o. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _DASHARO_OPTIONS_H_
+#define _DASHARO_OPTIONS_H_
+
+#define DASHARO_OPTION_ROM_POLICY_DISABLE_ALL  0
+#define DASHARO_OPTION_ROM_POLICY_ENABLE_ALL   1
+#define DASHARO_OPTION_ROM_POLICY_VGA_ONLY     2
+
+#endif
