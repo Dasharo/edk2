9a96388208026c5dae04b37bd532d0799f5ee42d
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-09-26 12:37:11 +0200

    MdeModulePkg: Control pre-installed boot options creation with PCD
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
+++ MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
@@ -2982,7 +2982,10 @@ BmEnumerateBootOptions (
 
   *BootOptionCount = 0;
 
-  BootOptions = BmEnumeratePreInstalledBootOptions(BootOptionCount);
+  if (PcdGetBool (PcdCreatePreInstalledBootOptions))
+    BootOptions = BmEnumeratePreInstalledBootOptions(BootOptionCount);
+  else
+    BootOptions = NULL;
 
   //
   // Parse removable block io followed by fixed block io
