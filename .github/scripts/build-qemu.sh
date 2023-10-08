81d0015e56b9ca1e836387cc0bf86e7ade4cd5d2
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-10-08 11:18:08 +0200

    .github/workflows/build.yml: add for buiding OVMF firmware
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ .github/scripts/build-qemu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+make -C BaseTools
+source ./edksetup.sh
+
+export EDK2_PLATFORMS_PATH="$WORKSPACE/edk2-platforms"
+export PACKAGES_PATH="$WORKSPACE:\
+$EDK2_PLATFORMS_PATH/Platform/Intel:\
+$EDK2_PLATFORMS_PATH/Silicon/Intel:\
+$EDK2_PLATFORMS_PATH/Features/Intel:\
+$EDK2_PLATFORMS_PATH/Features/Intel/Debugging:\
+$EDK2_PLATFORMS_PATH/Features/Intel/Network:\
+$EDK2_PLATFORMS_PATH/Features/Intel/OutOfBandManagement:\
+$EDK2_PLATFORMS_PATH/Features/Intel/PowerManagement:\
+$EDK2_PLATFORMS_PATH/Features/Intel/SystemInformation:\
+$EDK2_PLATFORMS_PATH/Features/Intel/UserInterface"
+
+build -a IA32 -a X64 -t GCC5 -b RELEASE -p OvmfPkg/OvmfPkgX64.dsc
+build -a IA32 -a X64 -t GCC5 -b DEBUG -p OvmfPkg/OvmfPkgX64.dsc
