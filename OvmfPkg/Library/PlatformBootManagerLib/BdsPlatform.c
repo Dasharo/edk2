e3d672634b0cbfe300274ab46868818cbad879ef
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-10-11 00:10:42 +0200

    OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c: print setup keys
    Our tests expect these strings to be there, as they are for the
    UefiPayloadPkg as well.
    
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
+++ OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
@@ -1475,6 +1475,12 @@ PlatformBootManagerAfterConsole (
 
   DEBUG ((EFI_D_INFO, "PlatformBootManagerAfterConsole\n"));
 
+  // This is probably the earliest we can print this, as before the console is
+  // not ready yet.
+  Print(L"ESC to enter Setup\n");
+  Print(L"F2 to enter Boot Manager Menu\n");
+  Print(L"ENTER to boot directly\n");
+
   if (PcdGetBool (PcdOvmfFlashVariablesEnable)) {
     DEBUG ((EFI_D_INFO, "PlatformBdsPolicyBehavior: not restoring NvVars "
       "from disk since flash variables appear to be supported.\n"));
