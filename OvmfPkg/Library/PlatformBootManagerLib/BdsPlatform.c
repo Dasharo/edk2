0be6586d72792820dedd2eba43e8db4f2e0345c8
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-10-11 15:26:27 +0200

    OvmfPkg: swap ESC and F2 mapping
    We want the following:
    - ESC - one time boot menu
    - F2 - setup menu
    
    See the discussion in Github:
    https://github.com/Dasharo/edk2/pull/91#issuecomment-1757624392
    
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
+++ OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
@@ -563,7 +563,7 @@ PlatformRegisterOptionsAndKeys (
              NULL,
              (UINT16)OptionNumber,
              0,
-             &F2,
+             &Esc,
              NULL
              );
   ASSERT (Status == EFI_SUCCESS || Status == EFI_ALREADY_STARTED);
@@ -571,7 +571,7 @@ PlatformRegisterOptionsAndKeys (
              NULL,
              (UINT16)BootOption.OptionNumber,
              0,
-             &Esc,
+             &F2,
              NULL
              );
   ASSERT (Status == EFI_SUCCESS || Status == EFI_ALREADY_STARTED);
@@ -2015,8 +2015,8 @@ PlatformBootManagerAfterConsole (
 
   // This is probably the earliest we can print this, as before the console is
   // not ready yet.
-  Print(L"ESC to enter Setup\n");
-  Print(L"F2 to enter Boot Manager Menu\n");
+  Print(L"F2 to enter Setup\n");
+  Print(L"ESC to enter Boot Manager Menu\n");
   Print(L"ENTER to boot directly\n");
 
   if (PcdGetBool (PcdOvmfFlashVariablesEnable)) {
