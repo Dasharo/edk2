43b7c321b8e9385d9517a49378fc02ff9a856c29
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-10-11 15:47:10 +0200

    .github/scripts/run-qemu.sh: run with graphic ON by default
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- .github/scripts/run-qemu.sh
+++ .github/scripts/run-qemu.sh
@@ -2,7 +2,7 @@
 
 cp ./Build/OvmfX64/RELEASE_GCC5/FV/OVMF_VARS.fd /tmp/OVMF_VARS.fd 
 
-qemu-system-x86_64 -machine q35,smm=on \
+q35_params="-machine q35,smm=on \
     -global driver=cfi.pflash01,property=secure,value=on \
     -drive if=pflash,format=raw,unit=0,file=Build/OvmfX64/RELEASE_GCC5/FV/OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=/tmp/OVMF_VARS.fd \
@@ -10,5 +10,10 @@ qemu-system-x86_64 -machine q35,smm=on \
     -global ICH9-LPC.disable_s3=1 \
     -qmp unix:/tmp/qmp-socket,server,nowait \
     -net none \
-    -serial telnet:localhost:1234,server,nowait \
-    -nographic
+    -serial telnet:localhost:1234,server,nowait"
+
+if [ "$1" == "nographic" ]; then
+    qemu-system-x86_64 $q35_params -nographic
+else
+    qemu-system-x86_64 $q35_params
+fi
