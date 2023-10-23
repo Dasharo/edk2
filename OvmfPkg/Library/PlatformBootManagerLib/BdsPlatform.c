509b26c4c5aa3102612a34c54ef013bcc7138777
Piotr Król <piotr.krol@3mdeb.com>
2023-10-23 23:01:51 +0200

    OvmfPkg: register ipxe.efi file as boot option
    * Add ipxe.efi to the firmware volume.
    * Based on NetBootEnable UEFI variable register or unregister iPXE as
      boot option.
    
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
+++ OvmfPkg/Library/PlatformBootManagerLib/BdsPlatform.c
@@ -1930,6 +1930,9 @@ PlatformBootManagerAfterConsole (
   )
 {
   EFI_BOOT_MODE  BootMode;
+  BOOLEAN        NetBootEnabled;
+  UINTN          VarSize;
+  EFI_STATUS     Status;
 
   DEBUG ((DEBUG_INFO, "PlatformBootManagerAfterConsole\n"));
 
@@ -2005,6 +2008,46 @@ PlatformBootManagerAfterConsole (
 
   if (RETURN_ERROR (RetStatus)) {
     ShellEnabled = TRUE;
+  }
+
+  VarSize = sizeof (NetBootEnabled);
+  Status = gRT->GetVariable (
+      L"NetworkBoot",
+      &gDasharoSystemFeaturesGuid,
+      NULL,
+      &VarSize,
+      &NetBootEnabled
+      );
+
+  //
+  // Register iPXE
+  //
+  if ((Status != EFI_NOT_FOUND) && (VarSize == sizeof(NetBootEnabled))) {
+    if (NetBootEnabled) {
+      DEBUG((DEBUG_INFO, "Registering iPXE boot option by variable\n"));
+      PlatformRegisterFvBootOption2 (PcdGetPtr (PcdiPXEFile),
+                                     (CHAR16 *) PcdGetPtr(PcdiPXEOptionName),
+                                     LOAD_OPTION_ACTIVE,
+                                     TRUE,
+                                     FALSE);
+    } else {
+        DEBUG((DEBUG_INFO, "Unregistering iPXE boot option by variable\n"));
+        PlatformUnregisterFvBootOption (PcdGetPtr (PcdiPXEFile),
+                                        (CHAR16 *) PcdGetPtr(PcdiPXEOptionName),
+                                        LOAD_OPTION_ACTIVE);
+    }
+  } else if ((Status == EFI_NOT_FOUND) && FixedPcdGetBool(PcdDefaultNetworkBootEnable)) {
+    DEBUG((DEBUG_INFO, "Registering iPXE boot option by policy\n"));
+    PlatformRegisterFvBootOption2 (PcdGetPtr (PcdiPXEFile),
+                                   (CHAR16 *) PcdGetPtr(PcdiPXEOptionName),
+                                   LOAD_OPTION_ACTIVE,
+                                     TRUE,
+                                   FALSE);
+  } else {
+    DEBUG((DEBUG_INFO, "Unregistering iPXE boot option\n"));
+    PlatformUnregisterFvBootOption (PcdGetPtr (PcdiPXEFile),
+                                    (CHAR16 *) PcdGetPtr(PcdiPXEOptionName),
+                                    LOAD_OPTION_ACTIVE);
   }
 
   //
