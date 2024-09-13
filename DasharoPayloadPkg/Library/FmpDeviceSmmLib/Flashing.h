42013dbf14202f27848d633430363e2c5e9c371b
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-09-13 21:50:27 +0300

    DasharoPayloadPkg/FmpDeviceSmmLib: prepare for data migration
    Introduce and integrate MergeFirmwareImages() which will perform the
    migration.
    
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- DasharoPayloadPkg/Library/FmpDeviceSmmLib/Flashing.h
+++ DasharoPayloadPkg/Library/FmpDeviceSmmLib/Flashing.h
@@ -12,4 +12,19 @@ ReadCurrentFirmware (
   VOID
   );
 
+/**
+  Migrates data from current firmware to new image before it's written.
+
+  @param[in] Current  Current image used as a source of data.
+  @param[in] New      New image which gets patched.
+
+  @return NULL  On error.
+**/
+VOID *
+EFIAPI
+MergeFirmwareImages (
+  IN CONST VOID  *Current,
+  IN CONST VOID  *New
+  );
+
 #endif // FLASHING_H__
