c7ee5f193dc92ed89c59687cdc685362a05d2d40
Michał Kopeć <michal.kopec@3mdeb.com>
2025-04-07 13:59:41 +0200

    Revert "UefiCpuPkg: Produce EFI memory attributes protocol"
    This protocol, introduced in UEFI v2.10 is known to break booting some
    scenarios: https://edk2.groups.io/g/devel/topic/110601533#msg121220
    
    This includes booting Fedora 41 (either installer from usb or from disk,
    already installed).
    
    We do not claim to support UEFI 2.10 spec, only 2.8 at this point. So
    disable the protocol and consider re-enabling once all software is
    updated to 2.10 spec and works with MAP correctly.
    
    This reverts commit efaa102d006f242da52a17c81d824a5377da284b.
    
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- UefiCpuPkg/CpuDxe/CpuDxe.c
+++ UefiCpuPkg/CpuDxe/CpuDxe.c
@@ -1034,11 +1034,6 @@ InitializeCpu (
   ASSERT_EFI_ERROR (Status);
 
   //
-  // Install EFI memory attribute Protocol
-  //
-  InstallEfiMemoryAttributeProtocol (mCpuHandle);
-
-  //
   // Refresh GCD memory space map according to MTRR value.
   //
   RefreshGcdMemoryAttributes ();
