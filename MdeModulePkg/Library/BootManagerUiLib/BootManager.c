47575fada68cb2e6b37ece1c675aa47644d5c5a0
Michał Żygowski <michal.zygowski@3mdeb.com>
2025-07-03 10:07:05 +0200

    treewide: Pass string literals in SetupResetReminder
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- MdeModulePkg/Library/BootManagerUiLib/BootManager.c
+++ MdeModulePkg/Library/BootManagerUiLib/BootManager.c
@@ -305,8 +305,6 @@ BmSetupResetReminder (
   )
 {
   EFI_INPUT_KEY                           Key;
-  CHAR16                                  *StringBuffer1;
-  CHAR16                                  *StringBuffer2;
   EFI_STATUS                              Status;
   EDKII_FORM_BROWSER_EXTENSION2_PROTOCOL  *FormBrowserEx2;
 
@@ -318,21 +316,17 @@ BmSetupResetReminder (
   // check any reset required change is applied? if yes, reset system
   //
   if (!EFI_ERROR (Status) && FormBrowserEx2->IsResetRequired ()) {
-    StringBuffer1 = AllocateZeroPool (MAX_STRING_LEN * sizeof (CHAR16));
-    ASSERT (StringBuffer1 != NULL);
-    StringBuffer2 = AllocateZeroPool (MAX_STRING_LEN * sizeof (CHAR16));
-    ASSERT (StringBuffer2 != NULL);
-    StrCpyS (StringBuffer1, MAX_STRING_LEN, L"Configuration changed. Reset to apply it Now.");
-    StrCpyS (StringBuffer2, MAX_STRING_LEN, L"Press ENTER to reset");
     //
     // Popup a menu to notice user
     //
     do {
-      CreatePopUp (EFI_LIGHTGRAY | EFI_BACKGROUND_BLUE, &Key, StringBuffer1, StringBuffer2, NULL);
+      CreatePopUp (
+        EFI_LIGHTGRAY | EFI_BACKGROUND_BLUE,
+        &Key,
+        L"Configuration changed. Reset to apply it Now.",
+        L"Press ENTER to reset",
+        NULL);
     } while (Key.UnicodeChar != CHAR_CARRIAGE_RETURN);
-
-    FreePool (StringBuffer1);
-    FreePool (StringBuffer2);
 
     gRT->ResetSystem (EfiResetCold, EFI_SUCCESS, 0, NULL);
   }
